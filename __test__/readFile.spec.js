f13cbb7276704e47b9fb849e76233234514df0e3
Cary Chen <chenzehui1@xiaomi.com>
2019-06-26 14:20:29 +0800

    chore: update readFile，add coverage

--- __test__/readFile.spec.js
+++ __test__/readFile.spec.js
@@ -4,11 +4,11 @@ import path from 'path'
 describe('readFile function', () => {
   it('works with async/await', async () => {
     expect.assertions(1)
-    await expect(readFile(path.resolve(__dirname, '../src/sum.js'))).resolves.toMatch(/function\ssum\s\(x1, x2\)\s/)
+    await expect(readFile(path.resolve(__dirname, './readFile.spec.js'))).resolves.toMatch(/readFile\sfunction/)
   })
 
   it('tests error with rejects', async () => {
     expect.assertions(1)
-    await expect(readFile({})).rejects.toThrow()
+    await expect(readFile('')).rejects.toThrow()
   })
 })
